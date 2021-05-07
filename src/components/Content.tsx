8553125181786bdf3bd34f9a3e495aefb0745a85
Augusto Hirt Farias <augustokukimusic@gmail.com>
2021-05-07 00:26:27 -0300

    envio do desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,53 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import "./styles/content.scss";
+import "./styles/global.scss";
+
+interface IContentProps {
+  selectedGenre: IGenre;
+  movies: MovieProps[];
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IGenre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function Content({ selectedGenre, movies }: IContentProps) {
+  return (
+    <>
+      <div className="container">
+        <header>
+          <span className="category">
+            Categoria:<span> {selectedGenre.title}</span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
